207ec09f82848ecffe4e73065170499c0e8a56b2
tuneum <ahdyke@yahoo.com>
2018-07-25 20:54:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import random
 
 im = Image.new('RGBA', (500,500))
 imWidth, imHeight = im.size
+draw = ImageDraw.Draw(im)
+px = 10
+rows = imWidth / px
+cols = imHeight / px
+
 im.save('imagebase.png')
 
 red = ImageColor.getcolor('red', 'RGBA')
@@ -10,7 +15,7 @@ blue = ImageColor.getcolor('blue', 'RGBA')
 
 filename = 'generatedImg'
 num = 0
-for i in range(5):
+for i in range(1):
 
     for x in range(imWidth):
         for y in range(imHeight):
@@ -20,12 +25,20 @@ for i in range(5):
 
             if y < ((imHeight/2) - horizon):
 
-                if state == 0:
-                    im.putpixel((x,y), (210,210,210,50))
-                else:
-                    im.putpixel((x,y),(210,210,210))
+                path_pts = [(x,y)]
+
+                for i in range(random.randrange(-100,100)):
+                    if x < imWidth and y < imHeight:
+                        x2 = x + 1 
+                        y2 = y + 1
+                        path_pts.append((x2,y2))
+
+                #if state == 0:
+                    #draw.line(path_pts, (210,210,210,50))
+                #else:
+                    draw.line(path_pts,(210,210,210),10)
+
             else:
-            
                 if state == 0:
                     im.putpixel((x,y),red)
                 else:
