2b2c86fdbd6a43b5391636fa95b744faf9ed8bf5
tuneum <ahdyke@yahoo.com>
2018-07-28 07:10:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ draw = ImageDraw.Draw(im)
 
 
 
-for i in range(2):
+for i in range(1):
 
 
     for x in range(imWidth):
@@ -28,29 +28,38 @@ for i in range(2):
 
             draw.rectangle((x, y, imWidth, imHeight), (135,206,235))
        
+            if y < ((imHeight/2) - horizon):
+                path_pts = [(x,y)]
+                for i in range(random.randrange(-100,100)):
+                    if x < imWidth and y < imHeight:
+                        x2 = x + 1 
+                        y2 = y + 1
+                        path_pts.append((x2,y2))
+                 #if state == 0:
+                    #draw.line(path_pts, (210,210,210,50))
+                #else:
+                    draw.line(path_pts,(135,206,235),10)
 
             if y > ((imHeight/2) - horizon):
 
                 randomize = random.randint(-30, 30)
-                lightest = 400 + randomize
-                mid = 450 + randomize
+                px_size = random.randint(0,2)
+                #lightest = 400 + randomize
+                #mid = 450 + randomize
 
                 if state == 1:#shades of red
-                    if y <= lightest:
-                        draw.rectangle((x, y, x + 100, y + 100),(255,99,71))
+                    #if y <= lightest:
+                     #   draw.rectangle((x, y, x + 100, y + 100),(255,99,71))
 
-                    elif y <= mid:
-                        draw.rectangle((x,y,x+100,y+100), (229,89,63))
+                    #elif y <= mid:
+                     #   draw.rectangle((x,y,x+100,y+100), (229,89,63))
                     
-                    else:
-                        draw.rectangle((x,y,x+100,y+100), (204,79,56))
+                    #else:
+                    draw.rectangle((x,y,x+px_size,y+px_size), (204,79,56))
                         
                 if state == 0: #shades of blue
-                    if y >= mid:
-                        draw.rectangle((x,y,x+100,y+100), (121,185,211)) 
-
-                    else:
-                        draw.rectangle((x,y,x+100,y+100), (108,164,188))                     
+                   
+                    draw.rectangle((x,y,x+100,y+100), blue)                     
 
     num += 1
     n = str(num)
